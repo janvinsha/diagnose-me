c74982afee8fc93d91ef7aa0104615425c8bbc2e
Janvinsha <janvinsha@gmail.com>
2023-05-29 17:43:34 +0100

    fix: some changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,11 +57,11 @@ const Home: NextPage = () => {
   };
 
   return (
-    <div className="flex h-screen flex-col p-10 items-center gap-6">
+    <div className="flex h-screen flex-col py-4 items-center gap-2 md:gap-6">
       <MetaTags title="App" />
       <h1 className="text-xl font-semibold">DIAGNOSE ME</h1>
-      <div className="flex w-3/5 h-5/6 border-r-2 gap-10 flex-col md:flex-row">
-        <div className="flex flex-col bg-[#36152D] h-full w-full rounded-lg p-8 text-white text-sm justify-between">
+      <div className="flex w-5/5 h-5/6 border-r-2 gap-10 flex-col md:flex-row md:w-3/5">
+        <div className="flex flex-col bg-[#36152D] h-full w-full rounded-lg p-4 md:p-8 text-white text-sm justify-between">
           <div className="flex flex-col overflow-y-scroll h-full gap-4 scroll-smooth ">
             {[
               {
@@ -103,7 +103,7 @@ const Home: NextPage = () => {
               </div>
             )}
           </div>
-          <div className="flex gap-2 pt-4">
+          <div className="flex gap-2 pt-4 ">
             <Input
               placeholder="Enter reply"
               className=" border-[#FFFFFF] p-2 border-opacity-20 text-black"
